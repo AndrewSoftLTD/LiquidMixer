b5eb5eab2513ec556e5bb2603f612d3207451d6e
jakabandras <jakabandras210@gmail.com>
2021-04-03 19:15:59 +0200

    Update platformio.ini

---
+++ src/liquidmixer.h
@@ -0,0 +1,1 @@
+#define MENU_BUTTON         5;
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,16 @@
 #include <Arduino.h>
+#include <OneWire.h>
+#include "DS18B20.h"
+#include "liquidmixer.h"
+#include "EEPROM.h"
+
+OneWire sens(10);
+//DS18B20 sensor(&sens);
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
+
 }
 
 void loop() {
