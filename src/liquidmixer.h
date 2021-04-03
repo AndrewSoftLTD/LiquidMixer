f45909d74cd92747fd71dd78a29f28034825fc6a
jakabandras <jakabandras210@gmail.com>
2021-04-03 22:39:41 +0200

    Haladok 1

--- src/liquidmixer.h
+++ src/liquidmixer.h
@@ -1,1 +1,10 @@
-#define MENU_BUTTON         5;
+#define MENU_BUTTON         5
+#define LOW_ALARM 20
+#define HIGH_ALARM 25
+
+#define NO_TEMP "Nincs hőmérő!"
+#define ERR_ERRORTEXT "HIBA:"
+
+#define STR_TEMPER "Hőmérséklet:"
+
+#define MST_
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,34 @@
 #include <Arduino.h>
 #include <OneWire.h>
-#include "DS18B20.h"
+#include <DS18B20.h>
 #include "liquidmixer.h"
 #include "EEPROM.h"
+#include "LCDIC2.h"
+#include "Encoder.h"
 
-OneWire sens(10);
-//DS18B20 sensor(&sens);
+DS18B20 ds(2);
+LCDIC2 lcd(0x27,16,2);
+uint8_t address[] = {40, 250, 31, 218, 4, 0, 0, 52};
+uint8_t selected;
+uint8_t minc = 38;
+uint8_t maxc = 42;
+bool instLCD = false;
+Encoder myEnc(5,6) // Encoder lábak
+
 
 void setup() {
   // put your setup code here, to run once:
+  instLCD = lcd.begin();
   Serial.begin(115200);
+  if (selected) {
+    ds.setAlarms(minc, maxc);
+  } else {
+    if (instLCD) {
+      lcd.print(NO_TEMP);
+    } else {
+      Serial.println(NO_TEMP);
+    }
+  }
 
 }
 
