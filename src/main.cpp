a3ffe945e003b88eb3e0b282193f923428f9734f
jakabandras <jakabandras210@gmail.com>
2021-05-31 11:20:10 +0200

    mnu1

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #include <AxisJoystick.h>
+#include <AccelStepper.h>
 
 #include <LcdMenu.h>
 
@@ -14,20 +15,35 @@
 
 Joystick* joystick;
 
-extern MenuItem mainMenu[];
+void startMixer();
 
-MenuItem mainMenu[] = {ItemHeader(),
-                       MenuItem("Start service"),
-                       MenuItem("Connect to WiFi"),
-                       MenuItem("Settings"),
-                       MenuItem("Blink SOS"),
-                       ItemFooter()};
+extern MenuItem mainMenu[];
+extern MenuItem mnuSettings[];
+
+
+MenuItem mainMenu[] = {
+    ItemHeader(),
+    MenuItem("Keveres", startMixer, MENU_ITEM_COMMAND),
+    MenuItem("Idozito"),
+    ItemSubMenu("Beallitasok",mnuSettings),
+    MenuItem("Reset"),
+    ItemFooter()
+};
+
+MenuItem mnuSettings[] = {
+    ItemHeader(mainMenu),
+    MenuItem("Forgas irany"),
+    MenuItem("Sebesseg"),
+    MenuItem("Homerseklet"),
+    MenuItem("Valtott irany"),
+    ItemFooter()
+};
 
 LcdMenu menu(LCD_ROWS, LCD_COLS);
 
-Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, 4, 4);
 
 void setup() { 
+    Serial.begin(9600);
     menu.setupLcdWithMenu(0x27, mainMenu); 
     joystick = new AxisJoystick(SW_PIN, VRX_PIN, VRY_PIN);
 }
@@ -37,7 +53,29 @@ void loop() {
      * IMPORTANT: You must call this function for the notification to take time
      * into account
      */
+
     menu.updateTimer();
+    Joystick::Move move = joystick->singleRead();
+
+    switch(move) {
+        case Joystick::Move::UP :
+            menu.up();
+            break;
+        case Joystick::Move::DOWN:
+            menu.down();
+            break;
+        case Joystick::Move::LEFT:
+            menu.back();
+            break;
+        case Joystick::Move::RIGHT:
+            menu.enter();
+            break;
+        case Joystick::Move::PRESS:
+            menu.enter();
+            break;    
+        case Joystick::Move::NOT:
+            break;
+    }
 
     // Listen to key
 //    char key = keypad.getKey();
@@ -52,17 +90,21 @@ void loop() {
 //            break;
 //        case 'C':
 //            menu.enter();
-            break;
-        case 'D':
-            menu.back();
-            break;
-        case '#':
-            // When the # key is pressed, the message displays for two seconds
-            showNotification();
-            break;
-        default:
-            break;
-    }
+//            break;
+//        case 'D':
+//            menu.back();
+//            break;
+//        case '#':
+//            // When the # key is pressed, the message displays for two seconds
+//            showNotification();
+//            break;
+//        default:
+//            break;
+//    }
+
+}
+void startMixer() {
+    Serial.println("Keverés indul....");
 }
 
 void showNotification() { menu.displayNotification("Success", 2000); }
